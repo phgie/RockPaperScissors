fe1e9d089f16c7af85f603cb1d8d43c0555018b9
Philipp Giesen <philipp.giesen@gmail.com>
2019-03-09 17:09:36 +0100

    changed Spiel

--- src/rpsframework/turnier/Spiel.java
+++ src/rpsframework/turnier/Spiel.java
@@ -4,12 +4,17 @@ import impl.Spieler;
 import rpsframework.basis.Duell;
 import rpsframework.basis.Symbol;
 
+/**
+ * Ein Spiel ist eine Begegnung zwischen zwei Spielern und laeuft ueber eine vorgegebene Zahl von Runden. Jede Runde ist
+ * ein Duell, dessen Ausgang abgefragt werden kann
+ */
 public class Spiel {
 
     private Spieler spieler1;
     private Spieler spieler2;
 
-    private int runden;
+    /* Die Duelle in diesem Spiel. Die Anzahl von Runden bestimmt die Laenge des Arrays */
+    private Duell[] duelle;
 
     private int spieler1siege;
     private int spieler2siege;
@@ -21,40 +26,57 @@ public class Spiel {
         this.spieler1 = spieler1;
         this.spieler2 = spieler2;
 
-        this.runden = runden;
+        this.duelle = new Duell[runden];
     }
 
+    /**
+     * Startet das Spiel und loest die einzelnen Begegnungen aus
+     */
     public void starteSpiel() {
 
         Symbol spieler1symbol;
         Symbol spieler2symbol;
 
-        for (int i = 0; i < runden; i++) {
+        for (int i = 0; i < duelle.length; i++) {
 
             spieler1symbol = spieler1.gibSymbol();
             spieler2symbol = spieler2.gibSymbol();
 
-            Duell duell = new Duell(spieler1symbol, spieler2symbol);
-            String ergebnis = duell.gibErgebnis();
+            duelle[i] = new Duell(spieler1symbol, spieler2symbol);
+
+            switch (duelle[i].gibErgebnis()) {
+
+                case -1:
+                    //Spieler 2 gewinnt
+                    spieler2siege++;
+                    break;
+                case 0:
+                    //unentschieden
+                    unentschieden++;
+                    break;
+                case 1:
+                    //Spieler 1 gewinnt
+                    spieler1siege++;
+            }
         }
     }
 
-    //TODO unschoen ;-)
+    /**
+     * Gibt das Ergebnis für einen Spieler zurück.
+     * @param spieler
+     * @return
+     */
     public int gibErgebnis(Spieler spieler) {
 
         if (spieler == null) {
 
             return unentschieden;
+        } else if (spieler.equals(spieler2)) {
+
+            return spieler2siege
         } else if (spieler.equals(spieler1)) {
 
             return spieler1siege;
-        } else if (spieler.equals(spieler2)) {
-
-            return spieler2siege;
-        } else {
-
-            //doof
-            return -1;
         }
     }
 }
